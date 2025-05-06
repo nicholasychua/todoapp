071158baca15e0a716e582fef43f09519c433807
nicholasychua <nickc1810@gmail.com>
2025-05-05 22:35:46 -0700

    Remove hard-coded API keys; switch to env vars

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBNG9y5tg-LN24zNO3XP4Buq53W2BRBF9w",
-    authDomain: "todolistproject-121c0.firebaseapp.com",
-    projectId: "todolistproject-121c0",
-    storageBucket: "todolistproject-121c0.firebasestorage.app",
-    messagingSenderId: "814468920886",
-    appId: "1:814468920886:web:1d04dc7aa93c4b66b00a97",
-    measurementId: "G-LHBJ83RN4W"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
   };
   
 // Initialize Firebase
